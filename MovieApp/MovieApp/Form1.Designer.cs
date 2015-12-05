2f33a0441e9f63ba2d61c872762d18ad2c4b8e52
kraphtdinner <krapht@gmail.com>
2015-12-04 22:50:45 -0600

    Scatter plot

--- MovieApp/MovieApp/Form1.Designer.cs
+++ MovieApp/MovieApp/Form1.Designer.cs
@@ -28,6 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.trackBar2 = new System.Windows.Forms.TrackBar();
@@ -41,10 +56,14 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -164,11 +183,97 @@
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(193, 48);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.MarkerSize = 15;
+            series1.Name = "Action";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Legend = "Legend1";
+            series2.MarkerSize = 15;
+            series2.Name = "Adventure";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series3.Legend = "Legend1";
+            series3.MarkerSize = 15;
+            series3.Name = "Romance";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series4.Legend = "Legend1";
+            series4.MarkerSize = 15;
+            series4.Name = "Thriller";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series5.Legend = "Legend1";
+            series5.MarkerSize = 15;
+            series5.Name = "Mystery";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series6.Legend = "Legend1";
+            series6.MarkerSize = 15;
+            series6.Name = "Crime";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series7.Legend = "Legend1";
+            series7.MarkerSize = 15;
+            series7.Name = "Comedy";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series8.Legend = "Legend1";
+            series8.MarkerSize = 15;
+            series8.Name = "Western";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series9.Legend = "Legend1";
+            series9.MarkerSize = 15;
+            series9.Name = "Sci-Fi";
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series10.Legend = "Legend1";
+            series10.MarkerSize = 15;
+            series10.Name = "Animation";
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series11.Legend = "Legend1";
+            series11.MarkerSize = 15;
+            series11.Name = "Drama";
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series12.Legend = "Legend1";
+            series12.MarkerSize = 15;
+            series12.Name = "War";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Series.Add(series9);
+            this.chart1.Series.Add(series10);
+            this.chart1.Series.Add(series11);
+            this.chart1.Series.Add(series12);
+            this.chart1.Size = new System.Drawing.Size(1035, 558);
+            this.chart1.TabIndex = 13;
+            this.chart1.Text = "chart1";
+            this.chart1.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1277, 635);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -189,6 +294,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -209,6 +315,9 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
--- MovieApp/MovieApp/Form1.cs
+++ MovieApp/MovieApp/Form1.cs
@@ -17,22 +17,127 @@ namespace MovieApp {
 
         private void button1_Click(object sender, EventArgs e) {
             pictureBox1.Visible = false;
-            AdminPage admin = new AdminPage();
-            admin.Show();
+            //AdminPage admin = new AdminPage();
+            //admin.Show();
             //showAllInListBox();
             //MessageBox.Show("List count is:" + listBox1.Items.Count);
+
+            // Search(gets list of searchedMovies)
+            List<Movie> allMovies = Util.getAllMovies();
+            string title = "B";//get from slider
+            string actor = "A";//get from slider
+            string director = "C";//get from slider
+            List<string> cert = new List<string>(); //get from checkboxes
+
+            List<Movie> titleSearch = Util.searchByTitle(allMovies, title);
+            List<Movie> actorSearch = Util.searchByTitle(allMovies, actor);
+            List<Movie> directorSearch = Util.searchByTitle(allMovies, director);
+
+            List<Movie> searchedMovies = titleSearch; //just for testing
+            //List<Movie> searchedMovies = Util.getFinalList(titleSearch.Concat(actorSearch.Concat(directorSearch)).ToList(), genre);
+            // End of Search
+
+            // Generate ScatterPlot
+            chart1.Visible = true;
+            chart1.ChartAreas[0].AxisX.Minimum = 1920;
+            chart1.ChartAreas[0].AxisX.Maximum = 2020;
+            chart1.ChartAreas[0].AxisX.Interval = 20;
+
+            int point;
+            for (int i=0; i<searchedMovies.Count; i++)
+            {
+                if (searchedMovies[i].genres[0] == "Action"){
+                    point = this.chart1.Series["Action"].Points.AddXY(Int32.Parse(searchedMovies[i].year), Int32.Parse(searchedMovies[i].rating));
+                    this.chart1.Series["Action"].Points[point].ToolTip = searchedMovies[i].title;
+                }
+
+                if (searchedMovies[i].genres[0] == "Adventure")
+                {
+                    point = this.chart1.Series["Adventure"].Points.AddXY(Int32.Parse(searchedMovies[i].year), Int32.Parse(searchedMovies[i].rating));
+                    this.chart1.Series["Adventure"].Points[point].ToolTip = searchedMovies[i].title;
+                }
+
+                if (searchedMovies[i].genres[0] == "History")
+                {
+                    point = this.chart1.Series["History"].Points.AddXY(Int32.Parse(searchedMovies[i].year), Int32.Parse(searchedMovies[i].rating));
+                    this.chart1.Series["History"].Points[point].ToolTip = searchedMovies[i].title;
+                }
+
+                if (searchedMovies[i].genres[0] == "Romance")
+                {
+                    point = this.chart1.Series["Romance"].Points.AddXY(Int32.Parse(searchedMovies[i].year), Int32.Parse(searchedMovies[i].rating));
+                    this.chart1.Series["Romance"].Points[point].ToolTip = searchedMovies[i].title;
+                }
+
+                if (searchedMovies[i].genres[0] == "War")
+                {
+                    point = this.chart1.Series["War"].Points.AddXY(Int32.Parse(searchedMovies[i].year), Int32.Parse(searchedMovies[i].rating));
+                    this.chart1.Series["War"].Points[point].ToolTip = searchedMovies[i].title;
+                }
+
+                if (searchedMovies[i].genres[0] == "Drama")
+                {
+                    point = this.chart1.Series["Drama"].Points.AddXY(Int32.Parse(searchedMovies[i].year), Int32.Parse(searchedMovies[i].rating));
+                    this.chart1.Series["Drama"].Points[point].ToolTip = searchedMovies[i].title;
+                }
+
+                if (searchedMovies[i].genres[0] == "Animation")
+                {
+                    point = this.chart1.Series["Animation"].Points.AddXY(Int32.Parse(searchedMovies[i].year), Int32.Parse(searchedMovies[i].rating));
+                    this.chart1.Series["Animation"].Points[point].ToolTip = searchedMovies[i].title;
+                }
+
+                if (searchedMovies[i].genres[0] == "Sci-Fi")
+                {
+                    point = this.chart1.Series["Sci-Fi"].Points.AddXY(Int32.Parse(searchedMovies[i].year), Int32.Parse(searchedMovies[i].rating));
+                    this.chart1.Series["Sci-Fi"].Points[point].ToolTip = searchedMovies[i].title;
+                }
+
+                if (searchedMovies[i].genres[0] == "Western")
+                {
+                    point = this.chart1.Series["Western"].Points.AddXY(Int32.Parse(searchedMovies[i].year), Int32.Parse(searchedMovies[i].rating));
+                    this.chart1.Series["Western"].Points[point].ToolTip = searchedMovies[i].title;
+                }
+
+                if (searchedMovies[i].genres[0] == "Comedy")
+                {
+                    point = this.chart1.Series["Comedy"].Points.AddXY(Int32.Parse(searchedMovies[i].year), Int32.Parse(searchedMovies[i].rating));
+                    this.chart1.Series["Comedy"].Points[point].ToolTip = searchedMovies[i].title;
+                }
+
+                if (searchedMovies[i].genres[0] == "Crime")
+                {
+                    point = this.chart1.Series["Crime"].Points.AddXY(Int32.Parse(searchedMovies[i].year), Int32.Parse(searchedMovies[i].rating));
+                    this.chart1.Series["Crime"].Points[point].ToolTip = searchedMovies[i].title;
+                }
+
+                if (searchedMovies[i].genres[0] == "Mystery")
+                {
+                    point = this.chart1.Series["Mystery"].Points.AddXY(Int32.Parse(searchedMovies[i].year), Int32.Parse(searchedMovies[i].rating));
+                    this.chart1.Series["Mystery"].Points[point].ToolTip = searchedMovies[i].title;
+                }
+
+                if (searchedMovies[i].genres[0] == "Thriller")
+                {
+                    point = this.chart1.Series["Thriller"].Points.AddXY(Int32.Parse(searchedMovies[i].year), Int32.Parse(searchedMovies[i].rating));
+                    this.chart1.Series["Thriller"].Points[point].ToolTip = searchedMovies[i].title;
+                }
+            }
+            //End of Generate ScatterPlot
+
         }
 
         private void Form1_Load(object sender, EventArgs e) {
 
         }
 
-        public void showAllInListBox() {
-            List<Movie> movies = Util.getAllMovies();
-            for(int i = 0; i < movies.Count; i++) {
-                listBox1.Items.Add(movies[i]);
-            }
-            listBox1.EndUpdate();
-        }
+
+        //public void showAllInListBox() {
+        //    List<Movie> movies = Util.getAllMovies();
+        //    for(int i = 0; i < movies.Count; i++) {
+        //        listBox1.Items.Add(movies[i]);
+        //    }
+        //    listBox1.EndUpdate();
+        //}
     }
 }
